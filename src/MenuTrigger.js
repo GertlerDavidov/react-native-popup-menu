57697d302e31486094ad0fa41a7945ac68719024
rammuk <ram.mukmel@gmail.com>
2016-06-08 14:02:29 +0300

    Add icon

--- src/MenuTrigger.js
+++ src/MenuTrigger.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, TouchableHighlight, Text, Image } from 'react-native';
 import { debug } from './logger.js';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class MenuTrigger extends Component {
 
@@ -11,15 +12,17 @@ export default class MenuTrigger extends Component {
   }
 
   render() {
-    const { disabled, onRef, text, children, style, customStyles, ...other } = this.props;
+    const { disabled, onRef, text, children, style, image, customStyles, ...other } = this.props;
     const onPress = () => !disabled && this._onPress();
+
     return (
       <View ref={onRef} collapsable={false}>
         <TouchableHighlight onPress={onPress} style={[customStyles.triggerWrapper, style]}
           {...defaultTouchableStyles} {...customStyles.triggerTouchable}>
-          <View {...other}>
-            {image ? <Image style={customStyles.triggerImage} /> : ''}
-            {text ? <Text style={customStyles.triggerText}>{text}</Text> : children}
+          <View {...other}  style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+            <Icon name="expand-more" size={16} color={'#ffffff'} style={{marginRight:6,}} />
+            {image ? children[1] : null}
+            {text ? <Text style={customStyles.triggerText}>{text}</Text> : children[0]}
           </View>
         </TouchableHighlight>
       </View>
@@ -31,7 +34,7 @@ export default class MenuTrigger extends Component {
 MenuTrigger.propTypes = {
   disabled: React.PropTypes.bool,
   text: React.PropTypes.string,
-  image: React.PropTypes.string,
+  image: React.PropTypes.bool,
   onPress: React.PropTypes.func,
   customStyles: React.PropTypes.object,
 };
